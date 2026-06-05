e811af28c9bf798770edcf8d4b9f62c57dc76581
Srikanth Reddy <srikanthreddytec@gmail.com>
2026-06-06 03:53:22 +0530

    removed wrangler and open next as moving to pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true, // Required by Next.js for static HTML exports
+  },
 };
 
 export default nextConfig;
